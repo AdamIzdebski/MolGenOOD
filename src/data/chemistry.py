c1fd8a1e5240f0f211e155cb0eac6b6ecc25bda7
AdamIzdebski <adam.izdebski1@gmail.com>
2025-08-12 17:00:44 +0200

    Add fetching and standardizing SMILES from ZINC database
    - Created `chemistry.py` for SMILES standardization functions.
    - Added `constants.py` to define ZINC druglike tranches.
    - Implemented `fetch_smiles.py` to fetch SMILES data from the ZINC database, including error handling and progress tracking.

---
+++ src/data/chemistry.py
@@ -0,0 +1,22 @@
+import datamol as dm
+from typing import Optional
+import warnings
+
+
+def standardize_smiles(smiles: str) -> Optional[str]:
+    # Standardize and sanitize
+    try:
+        mol = dm.to_mol(smiles, sanitize=True)
+        mol = dm.fix_mol(mol)
+        mol = dm.sanitize_mol(mol)
+        mol = dm.standardize_mol(mol)
+        standardized_smiles = dm.to_smiles(mol)
+        if standardized_smiles is not None:
+            return standardized_smiles
+        else:
+            warnings.warn(f"Empty SMILES string for sequence {smiles}", UserWarning)
+            return None
+    except Exception as e:
+        warnings.warn(f"Error standardizing SMILES {smiles}: {e}", UserWarning)
+        return None
+    
